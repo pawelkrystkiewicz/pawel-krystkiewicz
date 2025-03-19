eaaa88875da01031a7bb529bde42415d3c1370f7
Paweł <pawelkrystkiewicz@outlook.com>
2025-03-19 14:44:34 +0100

    feat: add Footer, Navbar, and CustomMDX components for enhanced site structure and navigation

---
+++ app/components/Footer.tsx
@@ -0,0 +1,53 @@
+import config from '@/config'
+import Icons from './Icons'
+import Link from 'next/link'
+
+const { name, linkedin, github, repo } = config
+
+const items = [
+  {
+    href: '/rss',
+    text: 'rss',
+    icon: Icons.RSS,
+  },
+  {
+    href: linkedin,
+    text: 'linkedin',
+    icon: Icons.LinkedIn,
+  },
+  {
+    href: github,
+    text: 'github',
+    icon: Icons.Github,
+  },
+  {
+    href: repo,
+    text: 'view source',
+    icon: Icons.Repository,
+  },
+]
+
+export default function Footer() {
+  return (
+    <footer className="sm:flex sm:flex-col sm:items-center">
+      <ul className="text-sm mt-8 flex flex-col space-x-0 space-y-2 md:flex-row md:space-x-4 md:space-y-0 text-text-secondary">
+        {items.map(({ href, text, icon: Icon }) => (
+          <li key={href}>
+            <Link
+              className="flex items-center micro-interactions link-descrete gap-1"
+              rel="noopener noreferrer"
+              target="_blank"
+              href={href}
+              title={text}>
+              <Icon />
+              <span className="sm:hidden block">{text}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <p className="mt-8 text-neutral-600 dark:text-neutral-300 text-xs text-center">
+        {name} © {new Date().getFullYear()} MIT Licensed
+      </p>
+    </footer>
+  )
+}
---
+++ app/components/Nav.tsx
@@ -0,0 +1,37 @@
+import config from '@/config'
+import { NavigationItem, NavigationItemProps } from './NavigationItem'
+
+const lastName = config.name.split(' ').pop()?.toLocaleLowerCase()
+const { flags } = config
+const navItems: Record<string, Omit<NavigationItemProps, 'path'>> = {
+  '/': {
+    name: 'Home',
+  },
+  ...(flags.articles && {
+    '/articles': {
+      name: 'Articles',
+    },
+  }),
+  '/cv': {
+    name: 'CV',
+    downloadFilename: `cv-${lastName}`,
+  },
+}
+
+export function Navbar() {
+  return (
+    <aside className="-ml-[8px] mb-16 tracking-tight">
+      <div className="lg:sticky lg:top-20">
+        <nav
+          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          id="nav">
+          <div className="flex flex-row space-x-0 pr-10">
+            {Object.entries(navItems).map(([path, { name, downloadFilename }]) => {
+              return <NavigationItem key={path} path={path} name={name} downloadFilename={downloadFilename} />
+            })}
+          </div>
+        </nav>
+      </div>
+    </aside>
+  )
+}
