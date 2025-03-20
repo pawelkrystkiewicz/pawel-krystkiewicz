974f8989829a428f0c54a828548b7152487653d5
Paweł <pawelkrystkiewicz@outlook.com>
2025-03-20 09:29:35 +0100

    feat: add loading indicator to downloader component and update global styles

--- app/components/cv/Downloader.tsx
+++ app/components/cv/Downloader.tsx
@@ -2,20 +2,30 @@
 
 import { downloadPdf } from '@/app/utils/download-pdf'
 import Icons from '../Icons'
+import { useState } from 'react'
+import { Loader } from '../Loader'
+
 interface DownloaderProps {
   filename: string
   pagePath: string
 }
 
 export const Downloader = ({ filename, pagePath }: DownloaderProps) => {
-  const handleExport = () => downloadPdf({ path: pagePath, name: filename })
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleExport = async () => {
+    setIsLoading(true)
+    await downloadPdf({ path: pagePath, name: filename })
+    setIsLoading(false)
+  }
 
   return (
     <button
       className='text-primary cursor-pointer  hover:text-primary/80 flex items-center gap-1'
       onClick={handleExport.bind(null)}
+      disabled={isLoading}
     >
-      <Icons.Download /> Download
+      <Icons.Download /> Download {isLoading && <Loader />}
     </button>
   )
 }
