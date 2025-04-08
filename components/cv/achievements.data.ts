f09186c67946d2c3a3178fd5dc3c89c687971a5b
Paweł <pawelkrystkiewicz@outlook.com>
2025-04-08 10:49:58 +0200

    refactor: achievements language

--- components/cv/achievements.data.ts
+++ components/cv/achievements.data.ts
@@ -6,20 +6,21 @@ export default [
     project: 'Contract Builder',
     entries: [
       {
-        description:
-          "Initiated and pioneered the development of Contract Builder app. Today, it drives the <b>majority</b> of company's revenue and has been adopted by leading insurers in the UK and US. Each client contract is valued at <b>several million GBP annually</b>.",
+        description: `Spearheaded the development of Contract Builder app.
+        Today, it drives the <b>majority</b> of company's revenue and has been adopted by leading insurers in the UK and US.
+        Each client contract is valued at <b>several million GBP annually</b>.`,
       },
       {
-        description:
-          'the app has now major impact on the digital transformation of the London insurance market through integration with Placing Platform Limited (PPL), reducing contract creation time by <b>50%</b> and expediting MRCv3 adoption by <b>75%</b>.',
+        description: `The app has significantly transformed the London insurance market by integrating with Placing Platform Limited (PPL),
+        achieving a <b>50%</b> reduction in contract creation time and facilitating a <b>75%</b> increase in MRCv3 adoption.`,
         source: [
           {
             link: 'https://placingplatformlimited.com/news/integrated-digital-contract-capability-powered-by-artificial/',
-            text: "PPL's perspective",
+            text: `PPL's perspective`,
           },
           {
             link: 'https://artificial.io/company/blog/ppl-launches-integrated-digital-contract-capability-powered-by-artificial/',
-            text: "Artificial Labs' perspective",
+            text: `Artificial Labs' perspective`,
           },
         ],
       },
@@ -30,8 +31,7 @@ export default [
 
     entries: [
       {
-        description:
-          "Optimized company's frontends load times by <b>25%</b> just by optimizing MongoDB queries.",
+        description: `Optimized company's frontends load times by <b>25%</b> just by optimizing MongoDB queries.`,
       },
     ],
   },
